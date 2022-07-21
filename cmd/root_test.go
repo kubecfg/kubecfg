022cc6b3641ee8b648428f41c42884b2f3578080
Marko Mikulicic <mmikulicic@gmail.com>
2022-07-21 09:40:57 +0200

    fix: Use GroupKind to detect dupes

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -12,7 +12,19 @@ func TestReadObjsDuplicates(t *testing.T) {
 	}
 
 	_, err := readObjs(cmd, []string{filepath.FromSlash("../testdata/duplicates.jsonnet")})
-	if got, want := err.Error(), `duplicate resource /v1, Kind=ConfigMap, "myns", "foo"`; got != want {
+	if got, want := err.Error(), `duplicate resource ConfigMap, "myns", "foo"`; got != want {
+		t.Fatalf("got: %s, want: %s", got, want)
+	}
+}
+
+func TestReadObjsDuplicatesVersion(t *testing.T) {
+	cmd := RootCmd
+	if err := cmd.ParseFlags(nil); err != nil {
+		t.Fatal(err)
+	}
+
+	_, err := readObjs(cmd, []string{filepath.FromSlash("../testdata/duplicates_version.jsonnet")})
+	if got, want := err.Error(), `duplicate resource Ingress.networking.k8s.io, "myns", "foo"`; got != want {
 		t.Fatalf("got: %s, want: %s", got, want)
 	}
 }
