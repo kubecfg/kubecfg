4d1e6ee74da1b7317eb57e1ac15718c61106d5e5
Marko Mikulicic <mmikulicic@gmail.com>
2023-07-25 18:27:14 +0200

    fix: test short names and fix bug

--- pkg/kubecfg/pack_test.go
+++ pkg/kubecfg/pack_test.go
@@ -2,6 +2,8 @@ package kubecfg
 
 import (
 	"fmt"
+	"net/url"
+	"reflect"
 	"testing"
 )
 
@@ -22,6 +24,14 @@ func TestFindCommonPathPrefix(t *testing.T) {
 			[]string{"/foo/bar/a/b", "/foo/bar/a/c", "/foo/bar/c/a", "/fox/zar/c/a"},
 			"/",
 		},
+		{
+			[]string{"/foo/bar/file1.txt", "/foo/bar/file1.txt"},
+			"/foo/bar/",
+		},
+		{
+			[]string{"/foo/bar/file1.txt"},
+			"/foo/bar/",
+		},
 	}
 
 	for i, tc := range testCases {
@@ -32,4 +42,48 @@ func TestFindCommonPathPrefix(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestShortNames(t *testing.T) {
+	testCases := []struct {
+		urls            []string
+		rootURL         string
+		short           []string
+		shortEntrypoint string
+	}{
+		{
+			[]string{"file:///Users/mkm/tmp/dummy.txt", "file:///Users/mkm/tmp/shell.jsonnet"}, "file:///Users/mkm/tmp/shell.jsonnet",
+			[]string{"dummy.txt", "shell.jsonnet"}, "shell.jsonnet",
+		},
+		{
+			[]string{"file:///Users/mkm/tmp/shell.jsonnet"}, "file:///Users/mkm/tmp/shell.jsonnet",
+			[]string{"shell.jsonnet"}, "shell.jsonnet",
+		},
+	}
+
+	for i, tc := range testCases {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			var urls []*url.URL
+			for _, u := range tc.urls {
+				parsed, err := url.Parse(u)
+				if err != nil {
+					t.Fatal(err)
+				}
+				urls = append(urls, parsed)
+			}
+			rootURL, err := url.Parse(tc.rootURL)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			short, shortEntrypoint := shortNames(urls, rootURL)
+			if got, want := short, tc.short; !reflect.DeepEqual(got, want) {
+				t.Errorf("got: %q, want: %q", got, want)
+			}
+			if got, want := shortEntrypoint, tc.shortEntrypoint; got != want {
+				t.Errorf("got: %q, want: %q", got, want)
+			}
+		})
+	}
+
 }
